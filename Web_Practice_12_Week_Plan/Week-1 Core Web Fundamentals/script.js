f7ed81611800a9e4cd5c518a4d4f7610efc60a74
Prabhat Kumar <2012prabhat@gmail.com>
2025-10-25 22:57:30 +0530

    Grid Layout added

--- Web_Practice_12_Week_Plan/Week-1 Core Web Fundamentals/script.js
+++ Web_Practice_12_Week_Plan/Week-1 Core Web Fundamentals/script.js
@@ -1,5 +1,10 @@
-console.log("OBS recording tested")
-
-for(let i=0;i<5;i++){
-    console.log("Count: " + i)
-}
+let darkThemeBtn = document.getElementById("darkThemeBtn");
+darkThemeBtn.addEventListener("click",()=>{
+    if(document.body.classList.contains('dark')){
+      document.body.classList.remove('dark');
+      darkThemeBtn.innerText = "Enable Dark Theme"
+    }else{
+        document.body.classList.add('dark');
+        darkThemeBtn.innerText = "Enable Light Theme"
+    }
+})
