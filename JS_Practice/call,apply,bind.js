b4419d05783bf19b418d2eb756fd3eb95b8c4f1f
Prabhat Kumar <2012prabhat@gmail.com>
2026-01-23 22:57:20 +0530

    updated

--- JS_Practice/call,apply,bind.js
+++ JS_Practice/call,apply,bind.js
@@ -1,4 +1,23 @@
 
-function printMyName(){
-
+function printMyName(name,age){
+    console.log(name,age)
 }
+
+function printMyName2(...args){
+    let test = args
+    console.log(test)
+}
+
+function printMyName3(){
+    console.log(this.name)
+}
+
+printMyName.call({},"Prabhat",27)
+
+printMyName2.apply({},["Prabhat",27])
+
+const newFunc = printMyName3.bind({name:'Prabhat'})
+
+newFunc()
+
+
