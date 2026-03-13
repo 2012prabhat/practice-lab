4f21b212d55a69315a6408fd5248f4d9b57117dd
Prabhat Kumar <2012prabhat@gmail.com>
2026-03-13 22:41:55 +0530

    Pre Rendering added

---
+++ next/nextjs-dashboard/app/dashboard/loding.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div>
+      <div className="skeleton h-6 w-40 mb-4"></div>
+      <div className="skeleton h-4 w-80"></div>
+    </div>
+  );
+}
